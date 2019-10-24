0ba6be87ecd10efff9e17524ab380c629c9c27f9
Julia <emidiant1999@gmail.com>
2019-10-25 00:41:12 +0300

    fixed bugs, changed structure of document, added new graph

--- lab1_part1.py
+++ lab1_part1.py
@@ -1,171 +1,205 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import math as m
+import time
+start_time = time.time()
 
-dpi = 80
-fig = plt.figure(dpi=dpi, figsize=(512 / dpi, 384 / dpi))
-mpl.rcParams.update({'font.size': 10})
-
-plt.axis([10, 20, 0, 20])
-
-plt.title('Function graph')
-plt.xlabel('x')
-plt.ylabel('f(x)')
-
-# Вариант?
-# Пример f(x) = (x-15)^2+5, x in [2, 200]
+# Вариант 5
 
 
 def func(x):
-    return (x - 15) ** 2 + 5
+    return (x + 5) ** 4
 
 
-def fibonacci(n):
+def fib(n):
     return m.pow((1 + m.sqrt(5))/2, n) - m.pow((1 - m.sqrt(5))/2, n) / m.sqrt(5)
 
 
-# Входные значения
-a = 2
-b = 200
-eps = 0.001
-delta = eps / 2
+def visualisation(a, b):
+    # Визуализация
+    dpi = 80
+    fig = plt.figure(dpi=dpi, figsize=(512 / dpi, 384 / dpi))
+    mpl.rcParams.update({'font.size': 10})
 
-# Определение количества знаков после запятой и точности eps
-decimal = 0
-epsilon = eps
-while epsilon < 1:
-    epsilon *= 10
-    decimal += 1
+    plt.axis([-10, 15, 0, 100000])
 
-# Визуализация
-val_y = []
-val_x = []
-x0 = a
-while x0 < b:
-    val_y += [func(x0)]
-    val_x += [x0]
-    x0 += 0.1
+    plt.title('Function graph')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    val_y = []
+    val_x = []
+    x0 = a
+    while x0 < b:
+        val_y += [func(x0)]
+        val_x += [x0]
+        x0 += 0.1
 
-plt.plot(val_x, val_y, color='blue', linestyle='-', label='func')
+    plt.plot(val_x, val_y, color='blue', linestyle='-', label='func')
+    plt.legend(loc='upper right')
+    plt.grid(True)
+    fig.savefig('visualisation.png')
 
-# Метод дихотомии (деления отрезка пополам)
-right = b
-left = a
-counter = 0
 
-while round(right - left, decimal) > eps:
-    counter += 1
-    x1 = (left + right) / 2 - delta
-    x2 = (left + right) / 2 + delta
-    f1 = func(x1)
-    f2 = func(x2)
-    if f1 > f2:
-        left = x1
-    if f1 < f2:
-        right = x2
-    if f1 == f2:
-        left = x1
-        right = x2
+def dichotomy(a, b, eps):
+    # Метод дихотомии (деления отрезка пополам)
+    right = b
+    left = a
+    delta = eps / 2
+    counter = 0
 
-print('Dichotomy method:', round((left + right) / 2, 3))
+    while right - left > eps:
+        counter += 1
+        x1 = (left + right) / 2 - delta
+        x2 = (left + right) / 2 + delta
+        f1 = func(x1)
+        f2 = func(x2)
+        if f1 > f2:
+            left = x1 + delta
+        if f1 < f2:
+            right = x2 - delta
+    # Количество итераций
+    print('Dichotomy method:', (left + right) / 2)
+    iter_dich = m.log((b - a) / eps, m.e) / m.log(2, m.e)
+    print(' Iteration in Dichotomy method.', 'rule:', iter_dich, ', counter:', counter, '\n')
+    return counter
 
-# todo Количество итераций
-
-iterDich = m.log((b - a) / eps, m.e) / m.log(2, m.e)
-
-# todo counter и rule слишком разнятся
-print(' Iteration in Dichotomy method.', 'rule:', round(iterDich, 1), ', counter:', counter)
 
 # todo Метод золотого сечения. Дополнить тему с погрешностью (Неточное задание величины sqrt(5))
-# todo Вычислять функцию единожды
-right = b
-left = a
-counter = 0
-iterGold = m.log((b - a) / eps, m.e) / m.log(1.618, m.e)
 
-while round(right - left, decimal) > eps:
-    counter += 1
-    x1 = left + 0.381966011 * (right - left)
-    x2 = left + 0.618003399 * (right - left)
+
+def golden_ratio(a, b, eps):
+    right = b
+    left = a
+    counter = 0
+    iter_gold = m.log((b - a) / eps, m.e) / m.log(1.618, m.e)
+    psi = (1 + m.sqrt(5)) / 2
+    x1 = right - (right - left) / psi
+    x2 = left + (right - left) / psi
     f2 = func(x2)
     f1 = func(x1)
-    if f1 > f2:
-        left = x1
-    if f1 < f2:
-        right = x2
-    if f1 == f2:
-        left = x1
-        right = x2
+    while right - left > eps:
+        counter += 1
+        if f1 > f2:
+            left = x1
+            x1 = x2
+            x2 = left + (right - left) / psi
+            f1 = f2
+            f2 = func(x2)
+        else:
+            right = x2
+            x2 = x1
+            x1 = right - (right - left) / psi
+            f2 = f1
+            f1 = func(x1)
+    print('Golden ratio:', (left + right) / 2)
+    print(' Iteration in Golden ratio.', 'rule:', iter_gold, ', counter:', counter, '\n')
+    return counter
 
-print('Golden ratio:', round((left + right) / 2, 3))
-print(' Iteration in Golden ratio.', 'rule:', round(iterGold, 1), ', counter:', counter)
 
-# todo Метод Фибоначчи. Отполировать, проверить
-right = b
-left = a
-# F(n+2) > (b-a)/eps
-num = int(m.ceil(m.log((m.sqrt(5) * (right - left)) / (eps * (3 + m.sqrt(5))), (1 + m.sqrt(5)) / 2)))
-
-# x1 = left + 2 * (right - left) / (3 + m.sqrt(5))
-# x2 = left + 2 * (right - left) / (1 + m.sqrt(5))
-
-xx1 = left + fibonacci(num) * (right - left) / fibonacci(num + 2)
-xx2 = left + fibonacci(num + 1) * (right - left) / fibonacci(num + 2)
-
-for k in range(0, num):
-    xx1 = left + fibonacci(num - k + 1) * (right - left) / fibonacci(num - k + 3)
-    xx2 = left + fibonacci(num - k + 2) * (right - left) / fibonacci(num - k + 3)
+def fibonacci(a, b, eps):
+    right = b
+    left = a
+    num = int(m.ceil(m.log((2 * m.sqrt(5) * (right - left)) / (eps * (3 + m.sqrt(5))), (1 + m.sqrt(5)) / 2)))
+    xx1 = left + fib(num) * (right - left) / fib(num + 2)
+    xx2 = left + fib(num + 1) * (right - left) / fib(num + 2)
     f2 = func(xx2)
     f1 = func(xx1)
-    if f1 > f2:
-        left = xx1
-    if f1 < f2:
-        right = xx2
+    for k in range(1, num + 1):
+        if f1 > f2:
+            left = xx1
+            xx1 = xx2
+            f1 = f2
+            xx2 = left + fib(num - k + 2) * (right - left) / fib(num - k + 3)
+            f2 = func(xx2)
+        if f1 < f2:
+            right = xx2
+            xx2 = xx1
+            f2 = f1
+            xx1 = left + fib(num - k + 1) * (right - left) / fib(num - k + 3)
+            f1 = func(xx1)
+    print('Fibonacci method:', (left + right) / 2)
+    print(' Iterations in Fibonacci method:', num, '\n')
+    return num
 
-print('Fibonacci method:', round((left + right) / 2, 3))
-print(' Iterations in Fibonacci method:', num)
 
 # todo Поиск минимума функции на прямой. тут первая реализация, ещё подумаю как оптимизировать код.
-# todo ломается при изменении точности с 0.001 на 0.01!!!
 
-xk1 = 1  # может быть любым
-if func(xk1) > func(xk1 + delta):
-    left = xk1
-    right = xk1 + delta
-    h = delta
-else:
-    if func(xk1) > func(xk1 - delta):
-        right = xk1
-        left = xk1 - delta
-        h = - delta
+
+def find_min_on_line(eps):
+    delta = eps/2
+    xk1 = 1  # может быть любым
+    if func(xk1) > func(xk1 + delta):
+        left = xk1
+        right = xk1 + delta
+        h = delta
     else:
-        print(xk1, 'is minimum')
-        h = 0
-
-if h != 0:
-    h *= 2
-    xk2 = xk1 + h
-
-    while func(xk1) > func(xk2):
+        if func(xk1) > func(xk1 - delta):
+            right = xk1
+            left = xk1 - delta
+            h = - delta
+        else:
+            print(xk1, 'is minimum')
+            h = 0
+    if h != 0:
         h *= 2
-        xk1 = xk2
-        xk2 += h
+        xk2 = xk1 + h
+        while func(xk1) > func(xk2):
+            h *= 2
+            xk1 = xk2
+            xk2 += h
+        if xk1 - h/2 > xk2 - h:
+            left = xk2
+            right = xk1 - h/2
+        else:
+            right = xk2
+            left = xk1 - h / 2
+        print('Finding the minimum function on the line', '[', left, ';', right, ']')
 
-    if xk1 - h/2 > xk2 - h:
-        left = xk2 - h
-        right = xk1 - h/2
-    else:
-        right = xk2 - h
-        left = xk1 - h / 2
-    print('Finding the minimum function on the line', '[', round(left, 3), ';', round(right, 3), ']')
 
-# todo Поиск минимума функции n переменных в заданном направлении
+def iterations_dependence(a, b):
+    eps = []
+    iter_dich = []
+    iter_gold = []
+    iter_fib = []
+    i = 0
+    while i < 1:
+        i += 0.001
+        eps += [m.log(i, 10)]
+        iter_dich += [2 * dichotomy(a, b, i)]
+        iter_gold += [1 + golden_ratio(a, b, i)]
+        iter_fib += [1 + fibonacci(a, b, i)]
+    dpi = 80
+    fig = plt.figure(dpi=dpi, figsize=(512 / dpi, 384 / dpi))
+    mpl.rcParams.update({'font.size': 10})
 
-plt.legend(loc='upper right')
-plt.grid(True)
-fig.savefig('visualisation.png')
+    plt.axis([-3, 0, 0, 32])
 
-# Вопросы: 1. как распределяются варианты? (самостоятельно)
-#          2. использование примитивных библиотек? (использовать можно всё, что не реализует задачи напрямую)
-#          3. время и дни защиты (гугл таблица?) (скинет)
+    plt.title('Graph of the number of calculations minimized functions')
+    plt.xlabel('ln(eps)')
+    plt.ylabel('Iter(ln)')
 
+    plt.plot(eps, iter_dich, color='blue', linestyle='-', label='dich')
+    plt.plot(eps, iter_gold, color='red', linestyle='-', label='gold')
+    plt.plot(eps, iter_fib, color='green', linestyle='-', label='fib')
+    plt.legend(loc='upper right')
+    plt.grid(True)
+    fig.savefig('graphMinimizedCalculations.png')
+
+
+def main():
+    # Входные значения
+    a = -10
+    b = 15
+    eps = 0.001
+    visualisation(a, b)
+    dichotomy(a, b, eps)
+    golden_ratio(a, b, eps)
+    fibonacci(a, b, eps)
+    find_min_on_line(eps)
+
+    iterations_dependence(a, b)
+    print("--- %s seconds ---" % round(time.time() - start_time, 3))
+
+
+if __name__ == "__main__":
+    main()
