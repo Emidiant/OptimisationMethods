301802a439bef76ca21614f9168b89f82a320ef6
Julia <emidiant1999@gmail.com>
2019-10-26 10:36:01 +0300

    changed number of calculations in first method

--- lab1_part2.py
+++ lab1_part2.py
@@ -1,5 +1,6 @@
 import math as m
 from sympy import *
+import scipy.misc
 
 
 def func(x1, x2):
@@ -8,12 +9,21 @@ def func(x1, x2):
 
 def derivative_x1(x1, x2):
     return (2 * x2 - 2) * (-x1 * (1 - x2) + 1.5) + (2 * x2 ** 2 - 2) * (-x1 * (1 - x2 ** 2) + 2.25) + \
-           (2 * x2 ** 3 - 2) * ( -x1 * (1 - x2 ** 3) + 2.625)
+           (2 * x2 ** 3 - 2) * (-x1 * (1 - x2 ** 3) + 2.625)
 
 
 def derivative_x2(x1, x2):
     return 6 * x1 * x2 ** 2 * (-x1 * (1 - x2 ** 3) + 2.625) + 4 * x1 * x2 * (-x1 * (1 - x2 ** 2) + 2.25) + \
            2 * x1 * (-x1 * (1 - x2) + 1.5)
+
+
+def partial_derivative(func, var=0, point=[]):
+    args = point[:]
+
+    def wraps(x):
+        args[var] = x
+        return func(*args)
+    return scipy.misc.derivative(wraps, point[var], dx = 1e-3)
 
 
 def g(x1, x2, alpha):
@@ -23,7 +33,7 @@ def g(x1, x2, alpha):
 
 
 def find_min_on_line(x1, x2, eps):
-    delta = eps/2
+    delta = eps / 2
     xk1 = 1  # может быть любым
     if g(x1, x2, xk1) > g(x1, x2, xk1 + delta):
         left = xk1
@@ -45,13 +55,14 @@ def find_min_on_line(x1, x2, eps):
             xk1 = xk2
             xk2 += h
 
-        if xk1 - h/2 > xk2 - h:
+        if xk1 - h / 2 > xk2 - h:
             left = xk2
-            right = xk1 - h/2
+            right = xk1 - h / 2
         else:
             right = xk2
             left = xk1 - h / 2
-        print('Finding the minimum function on the line', '[', left, ';', right, ']')
+        #print('Finding the minimum function on the line',
+#              '[', left, ';', right, ']')
         borders = []
         borders += [left]
         borders += [right]
@@ -91,15 +102,19 @@ def golden_ratio(x0, y0, a, b, eps):
 
 def main():
     # Входные значения
-
     x1, x2 = symbols('x1 x2')
 
-    print(func(x1, x2).diff(x1))
-    print(func(x1, x2).diff(x2))
+    x1der = func(x1, x2).diff(x1)
+    x2der = func(x1, x2).diff(x2)
+    print(x1der)
+    print(x2der)
 
-    x1, x2 = 5, 5
+    x1der = lambda x, y: partial_derivative(func, 0, [x, y])
+    x2der = lambda x, y: partial_derivative(func, 1, [x, y])
+    origx1, origx2 = 100, 100
+    x1, x2 = origx1, origx2
 
-    eps = 0.0001
+    eps = 0.00001
 
     der1 = derivative_x1(x1, x2)
     der2 = derivative_x2(x1, x2)
@@ -111,6 +126,7 @@ def main():
     while m.fabs(f0 - f1) > eps:
         counter += 1
         print('Частные производные:', der1, der2)
+        print('Частные производные:', x1der(x1, x2), x2der(x1, x2))
         borders = find_min_on_line(x1, x2, eps)
         res_gold = golden_ratio(x1, x2, borders[0], borders[1], eps)
         print('g(alpha) =', g(x1, x2, res_gold[0]))
@@ -128,15 +144,39 @@ def main():
 
     print('Number of iterations = ', counter)
 
+    x1, x2 = origx1, origx2
+
+    der1 = x1der(x1, x2)
+    der2 = x2der(x1, x2)
+
+    counter = 0
+    f1 = func(x1, x2)
+    f0 = f1 - eps * 2
+
+    while m.fabs(f0 - f1) > eps:
+        counter += 1
+        borders = find_min_on_line(x1, x2, eps)
+        res_gold = golden_ratio(x1, x2, borders[0], borders[1], eps)
+        ans_alpha = res_gold[0]
+        f0 = f1
+        x1 -= ans_alpha * der1
+        x2 -= ans_alpha * der2
+        f1 = func(x1, x2)
+        der1 = x1der(x1, x2)
+        der2 = x2der(x1, x2)
+    print('')
+    print("Numerical calculation of derivative:")
+    print('x1 =', x1, 'x2 =', x2)
+    print('Number of iterations = ', counter)
+
 
 if __name__ == "__main__":
     main()
 
-
 # таблицы с результатами проведенных исследований, где должны быть отражены:
-    # начальное приближение x0,
-    # задаваемая точность,
-    # количество итераций,
-    # число вычислений целевой функции,
-    # найденная точка,
-    # значение функции в ней,
+# начальное приближение x0,
+# задаваемая точность,
+# количество итераций,
+# число вычислений целевой функции,
+# найденная точка,
+# значение функции в ней,
